3b6e7c0ee805a8dad7ecff575dabb9c93cc1e2f9
PureLin <13639820045@163.com>
2014-08-12 11:07:15 +0800

    firststep

---
+++ WebApplication1/WebApplication1/NVRender.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Common;
+using System.Data.Sql;
+using System.Data.SqlTypes;
+using System.Configuration;
+using NVelocity.App;
+using NVelocity.Runtime;
+using NVelocity;
+
+namespace Nvelocity
+{
+    public static class NVRender
+    {
+
+        public static string ReanderHtml(string templateName, object data ,string rootName)
+        {
+            VelocityEngine vltEngine = new VelocityEngine();
+            vltEngine.SetProperty(RuntimeConstants.RESOURCE_LOADER, "file");
+            vltEngine.SetProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH,
+             System.Web.Hosting.HostingEnvironment.MapPath("~/template"));
+            vltEngine.Init();
+
+            VelocityContext vltContext = new VelocityContext();
+            vltContext.Put(rootName, data);
+            Template vltTemplate = vltEngine.GetTemplate(templateName);
+            System.IO.StringWriter vltWriter = new System.IO.StringWriter();
+            vltTemplate.Merge(vltContext, vltWriter);
+
+            return vltWriter.GetStringBuilder().ToString();
+        }
+
+        public static string ReanderHtml(string templateName, object data)
+        {
+            return ReanderHtml(templateName, data, "data");
+        }
+
+    }
+}
