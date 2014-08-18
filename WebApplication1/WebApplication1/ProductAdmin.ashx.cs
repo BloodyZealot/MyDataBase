df83131401c7fd2703d65ef383591d1d53cf8042
PureLin <13639820045@163.com>
2014-08-18 15:23:03 +0800

    EDIT&ADD Product

--- WebApplication1/WebApplication1/ProductAdmin.ashx.cs
+++ WebApplication1/WebApplication1/ProductAdmin.ashx.cs
@@ -3,6 +3,7 @@ using SqlHelper;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -18,21 +19,76 @@ namespace WebApplication1
         {
             context.Response.ContentType = "text/html";
             DataTable table;
-            string html;
+            string html = "";
+            string name, msg, cid, imagename, path;
+            int id;
+            HttpPostedFile image;
             switch (context.Request["action"])
             {
                 case "edit":
-                    int id=Convert.ToInt32(context.Request["id"]);
+                    id = Convert.ToInt32(context.Request["id"]);
                     table = MySqlHelper.ExecuteDataTable("select * from products where id=@id",
                         new MySql.Data.MySqlClient.MySqlParameter("@id", id));
-                    if (table.Rows.Count !=1)
+                    if (table.Rows.Count != 1)
                         html = "Error! You have make a worng request!";
                     else
-                        html = NVRender.ReanderHtml("/admin/productedit.html", 
-                            new { title = "edit product", product = table.Rows[0] });
+                        html = NVRender.ReanderHtml("/admin/productedit.html",
+                            new { title = "edit product", baseaction = "edit", product = table.Rows[0] });
+                    break;
+                case "editlaunch":
+                    id = Convert.ToInt32(context.Request["productid"]);
+                    name = context.Request["productname"];
+                    msg = context.Request["productmsg"];
+                    cid = context.Request["productcategoryid"];
+                    image = context.Request.Files["productimage"];
+                    if (image.FileName != "")
+                    {
+                        imagename = DateTime.Now.ToString("yyyymmddhhmmssffff") + Path.GetExtension(image.FileName);
+                        path = context.Server.MapPath("~/uploadfile/");
+                        image.SaveAs(path + imagename);
+                        MySqlHelper.ExecuteNonQuery(@"update products set Name=@name,Msg=@msg,ImagePath=@image,CategoryID=@cid
+                                                                                    where ID=@id",
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@id", id),
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@name", name),
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@msg", msg),
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@image", "uploadfile/" + imagename),
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@cid", Convert.ToInt32(cid)));
+                    }
+                    else
+                    {
+                        MySqlHelper.ExecuteNonQuery(@"update products set Name=@name,Msg=@msg,CategoryID=@cid
+                                                                                    where ID=@id",
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@id", id),
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@name", name),
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@msg", msg),
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@cid", Convert.ToInt32(cid)));
+                    }
+                    context.Response.Redirect("ProductAdmin.ashx");
+                    break;
+                case "add":
+                    html = NVRender.ReanderHtml("/admin/productedit.html",
+                            new { title = "add product", baseaction = "add", product = new { Name = "", Msg = "", CategoryID = "" } });
+                    break;
+                case "addlaunch":
+                    name = context.Request["productname"];
+                    msg = context.Request["productmsg"];
+                    cid = context.Request["productcategoryid"];
+                    image = context.Request.Files["productimage"];
+                    imagename = DateTime.Now.ToString("yyyymmddhhmmssffff") + Path.GetExtension(image.FileName);
+                    path = context.Server.MapPath("~/uploadfile/");
+                    image.SaveAs(path + imagename);
+                    MySqlHelper.ExecuteNonQuery(@"insert into products (Name,Msg,ImagePath,CategoryID) values
+                                                                                (@name,@msg,@image,@cid)",
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@name", name),
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@msg", msg),
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@image", "uploadfile/" + imagename),
+                                                                                new MySql.Data.MySqlClient.MySqlParameter("@cid", Convert.ToInt32(cid)));
+                    context.Response.Redirect("ProductAdmin.ashx");
                     break;
                 default:
-                    table = MySqlHelper.ExecuteDataTable("select * from products");
+                    table = MySqlHelper.ExecuteDataTable(@"select products.*, productcategory.Name as 'CName'
+                                                                                        from products join productcategory
+                                                                                        on products.CategoryID=productcategory.ID");
                     html = NVRender.ReanderHtml("/admin/productslist.html", new { rows = table.Rows, title = "list" });
                     break;
             }
