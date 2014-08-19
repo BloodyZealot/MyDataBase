f770e410f72904964e05049c816c8a3e647ef4ca
PureLin <13639820045@163.com>
2014-08-19 10:53:17 +0800

    Front Product List AComplish

--- WebApplication1/WebApplication1/ProductList.ashx.cs
+++ WebApplication1/WebApplication1/ProductList.ashx.cs
@@ -18,15 +18,47 @@ namespace WebApplication1
         {
             context.Response.ContentType = "text/html";
             string html;
+            string category = context.Request["category"];
+            string limit="";
             DataTable table;
-            switch (context.Request["class"])
+            if (!string.IsNullOrEmpty(category))
             {
-                default:
-                    table = MySqlHelper.ExecuteDataTable("select * from products");
-                    break;
+                int cid = Convert.ToInt32(MySqlHelper.ExecuteScalar("select ID from productcategory " +
+                                                                                                   "where Name=@name",
+                                                           new MySql.Data.MySqlClient.MySqlParameter("@name", category)));
+                if (cid != 0)
+                    limit = " where products.CategoryID = " + cid;
+                else
+                    category = "";
             }
-
-            html = NVRender.ReanderHtml("products.html", new { title = "产品展示" ,rows=table.Rows });
+            else
+                category = "";
+            string snum = context.Request["page"];
+            int pagenum;
+            if (string.IsNullOrEmpty(snum))
+                pagenum = 1;
+            else
+                pagenum = Convert.ToInt32(snum);
+            int productnum = Convert.ToInt32(MySqlHelper.ExecuteScalar("select count(*) from products " + limit));
+            int pagecount = (int)Math.Ceiling(productnum / 12.0);
+            int[] array = new int[pagecount];
+            for (int i = 0; i != pagecount; ++i)
+                array[i] = i + 1;
+            table = MySqlHelper.ExecuteDataTable("select products.*, productcategory.Name as 'CName' " +
+                                                                                " from products join productcategory " +
+                                                                                " on products.CategoryID=productcategory.ID " + limit +
+                                                                                " order by products.ID limit @pagenum,12 ",
+                                                     new MySql.Data.MySqlClient.MySqlParameter("@pagenum", (pagenum - 1) * 12));
+            html = NVRender.ReanderHtml("products.html", new
+                                                                                               {
+                                                                                                   title = "产品展示",
+                                                                                                   rows = table.Rows,
+                                                                                                   array = array,
+                                                                                                   currentpage = pagenum,
+                                                                                                   maxpage = pagecount,
+                                                                                                   productnum = productnum,
+                                                                                                   category=category
+                                                                                               });
             context.Response.Write(html);
         }
 
---
+++ WebApplication1/WebApplication1/scripts/ajax.js
@@ -0,0 +1,15 @@
+﻿function AjaxHandler(requesturl, onsuccess) {
+    var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xmlhttp.open("POST", requesturl, true);
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4) {
+            if (xmlhttp.status == 200) {
+                onsuccess(xmlhttp.responseText);
+            }
+            else {
+                alert("AJAX服务器返回错误！");
+            }
+        }
+    }
+    xmlhttp.send();
+}
