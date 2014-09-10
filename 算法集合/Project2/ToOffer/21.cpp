cedbcd275d9c0112895b8640a204045c0d169403
PureLin <13639820045@163.com>
2014-09-10 17:11:13 +0800

    TO offer

---
+++ 算法集合/Project2/ToOffer/21.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+/*Minstack 类：最小栈的实现
+/*使用两个栈，一个栈存储数据一个存储辅助的最小数值*/
+template<typename T>
+class minstack
+{
+public :
+	minstack() :realnums(), minnums(){};
+	/*push操作的特殊之处：
+	/*在存入数字栈的同时，判断这个数字是不是新的最小值，
+	/*如果是，就在辅助栈存入一个新的最小值，否则存入原来栈的最小值*/
+	void push(T value)
+	{
+		realnums.push(value);
+		//注意，minnums可能是空的
+		if (minnums.size()==0||value < minnums.top())
+			minnums.push(value);
+		else
+			minnums.push(minnums.top());
+	}
+	T top()
+	{
+		return realnums.top();
+	}
+	/*与普通栈相比，minstack多了一个获取最小数值的操作*/
+	T min()
+	{
+		return minnums.top();
+	}
+	void pop()
+	{
+		realnums.pop();
+		minnums.pop();
+	}
+	size_t size()
+	{
+		return realnums.size();
+	}
+private:
+	stack<T> realnums;
+	stack<T> minnums;
+};
+
+int main()
+{
+	minstack<int> mystack;
+	int n,temp;
+	char oper;
+	cin >> n;
+	for (int i = 0; i != n; ++i)
+	{
+		cin >> oper;
+		if (oper == 's')
+		{
+			cin >> temp;
+			mystack.push(temp);
+		}
+		else
+			mystack.pop();
+		if (mystack.size())
+			cout << mystack.min()<<endl;
+		else
+			cout << "NULL"<<endl;
+	}
+	return 0;
+}
--- 算法集合/Project2/源1.cpp
+++ 算法集合/Project2/源1.cpp
@@ -1,22 +1,26 @@
 #include<iostream>
-#include<cmath>
+#include<string>
+#include<vector>
+#include<typeinfo>
+
 using namespace std;
+
+struct data
+{
+	float f;
+};
 
 int main()
 {
-	int m,n;
-	cin>>m>>n;
-	int * a1=new int[m];
-	int * a2=new int[n];
-	for(int i=0;i!=m;++i)
-		cin>>a1[i];
-	for(int i=0;i!=n;++i)
-		cin>>a2[i];
-	int min=abs(a1[0]-a2[0]),temp;
-	for(int i=0;i!=m;++i)
-		for(int j=0;j!=n;++j)
-			if((temp=abs(a1[i]-a2[j]))<min)
-				min=temp;
-	cout<<min;
+	int a;
+	data b;
+	vector<int> c;
+	string d;
+	void* p = &c;
+	cout << typeid(a).name() << "\n raw " << typeid(a).raw_name() << endl;
+	cout << typeid(b).name() << "\n raw " << typeid(b).raw_name() << endl;
+	cout << typeid(c).name() << "\n raw " << typeid(c).raw_name() << endl;
+	cout << typeid(d).name() << "\n raw " << typeid(d).raw_name() << endl;
+	cout << typeid(p).name() << "\n raw " << typeid(p).raw_name() << endl;
 	return 0;
 }
