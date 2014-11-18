633e50740c70d79a6e3dabbe5cdaa52a3c4bf525
PureLin <13639820045@163.com>
2014-11-18 09:04:59 +0800

    pat

---
+++ 算法集合/Project2/PAT/pat-1046-unsloved.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+long long dist[10010];
+long long temp,sum,half;
+
+int main()
+{
+	int n;
+	int starter, ender;
+	cin >> n;
+	dist[0] = 0;
+	for (int i = 0; i != n; ++i)
+	{
+		cin >> temp;
+		dist[i + 1] = dist[i] + temp;
+	}
+	sum = dist[n];
+	half = sum / 2;
+	cin >> n;
+	for (int i = 0; i != n; ++i)
+	{
+		cin >> starter >> ender;
+		if (ender < starter)
+			swap(starter, ender);
+		temp = dist[ender - 1] - dist[starter - 1];
+		if (temp > half)
+			temp = sum - temp;
+		cout << temp << endl;
+	}
+	return 0;
+}
